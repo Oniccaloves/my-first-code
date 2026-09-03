ff156368d02a98cade3e246a518319b38f727ede
Oniccaloves <lethabomonyela31@gmail.com>
2026-09-03 22:41:54 +0200

    Update MyCode2.java

--- MyCode2.java
+++ MyCode2.java
@@ -6,14 +6,3 @@ System.out.println(city);
 System.out.prinln(year);
 }
 }
-public class MyCode2 {
-public static void main(String[] args) {
-String headline = "SA REAL NEWS - 3 Sept 2026";
-String story1 = "Springboks vs All Blacks at FNB Stadium - flyby cancelled";
-String story2 = "Rand is up - trading at 16.00 to the dollar";
-String city = "Johannesburg";
-System.out.println(headline)"
-System.out.printIn("News 1: " + story1);
-System.out.println("News 2: " + story2);
-}
-}
