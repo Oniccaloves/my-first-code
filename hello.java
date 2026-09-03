17c29162c6be5daea7d143038212af560cb70205
Oniccaloves <lethabomonyela31@gmail.com>
2026-09-03 21:35:54 +0200

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,7 @@
+public class Hello {
+public static void main(String[] args) {
+System.out.println("hello, i am lethabo");
+System.out.println("IT Software Dev student from Pretoria");
+System.out.println("this is my first Java file!");
+}
+}
