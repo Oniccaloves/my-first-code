da0655ce318251cab8d7af024e2c69dd43b5e938
Oniccaloves <lethabomonyela31@gmail.com>
2026-09-03 22:11:01 +0200

    Update MyCode2.java

--- MyCode2.java
+++ MyCode2.java
@@ -1,7 +1,7 @@
 public class MyCode2 {
 public static void main(String[] args) {
 String city="Johannesburg";
-int year="2026";
+int year=2026;
 System.out.printIn(city)
 System.out.printIn(year)
 }
