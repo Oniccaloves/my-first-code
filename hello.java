8d38f7599f61cd873ebfa95bd6fcd98eb1ce4010
Oniccaloves <lethabomonyela31@gmail.com>
2026-09-03 21:39:09 +0200

    Delete hello.java

--- hello.java
+++
@@ -1,7 +0,0 @@
-public class Hello {
-public static void main(String[] args) {
-System.out.println("hello, i am lethabo");
-System.out.println("IT Software Dev student from Pretoria");
-System.out.println("this is my first Java file!");
-}
-}
